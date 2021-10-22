1888add1b2fb0be4317d21d92bd50035e2972805
Mariam Alzaabi <mariam.mk.alzaabi@gmail.com>
2021-10-22 23:50:11 +0400

    Creating Recommendations Page

---
+++ SDP App/SDP App/ViewModels/RecommendationsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace SDP_App.ViewModels
+{
+	public class RecommendationsViewModel : BaseViewModel
+	{
+		public RecommendationsViewModel()
+		{
+			Title = "Recommendations";
+		}
+	}
+}
---
+++ SDP App/SDP App/Views/RecommendationsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SDP_App.Views
+{
+	public partial class RecommendationsPage : ContentPage
+	{
+		public RecommendationsPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
